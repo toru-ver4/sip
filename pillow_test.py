812a3b58cc04ee32cdc64a124fcefe862ba54989
Toru Yoshihara <toru.ver.11@gmail.com>
2015-11-03 21:34:54 +0900

    implemented half filter.

--- pillow_test.py
+++ pillow_test.py
@@ -10,7 +10,16 @@ import sys
 from PIL import Image
 
 if __name__ == '__main__':
-    img = Image.open('mikuru.jpg', 'r')
-    out = img.point(lambda i:i * 0.5)
-    out.save('half.jpg', 'JPEG')
+    img = Image.open('tabako.jpg', 'r')
+    pixels = img.load()
+    width, height = img.size
+
+    for xIdx in range(width):
+        for yIdx in range(height):
+            rrr = round(pixels[xIdx,yIdx][0] * 0.5)
+            ggg = round(pixels[xIdx,yIdx][1] * 0.5)
+            bbb = round(pixels[xIdx,yIdx][2] * 0.5)
+            pixels[xIdx,yIdx] = (rrr, ggg, bbb)
+            
+    img.save('tabako_half.jpg')
     
