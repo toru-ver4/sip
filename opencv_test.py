17be3f10ae9c1b2f517737b11b8de206e04f3c91
Toru Yoshihara <toru.ver.11@gmail.com>
2015-11-07 20:47:52 +0900

    - convert from 10bit dpx to 16bit tiff - apply function each pixels.

--- opencv_test.py
+++ opencv_test.py
@@ -6,30 +6,44 @@ helloqt.py
 PyQt5 で Hello, world!
 """
 
-FileName='aaa.tif' 
+File_name='bbb.dpx' 
 import os
 import sys
 import cv2
 import numpy as np
+from pgmagick import Image
+
+normalized_val_uint16 = 65535
 
 def gamma_func(val):
     return val * 0.5
 
+def conv_dpx10_to_tiff16(name):
+    root, ext = os.path.splitext(name)
+    out_name = root + ".tif"
+    cmd = "convert -depth 10" + " " + name + " " + "-depth 16" + " " + out_name
+    print(cmd)
+    os.system(cmd)
+
+    return out_name
 
 if __name__ == '__main__':
 
-    img = cv2.imread('tabako.jpg', cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
-    print(img[0])
+    tiff_image_name = conv_dpx10_to_tiff16(File_name)
 
-    img = img/255
+    img = cv2.imread(tiff_image_name, cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
 
-    for hhh, line in enumerate(img):
-        for vvv, pixel in enumerate(line):
-            for rgb, val in enumerate(pixel):
-                img[hhh][vvv][rgb] = gamma_func(val)
+    normalized_val = 2**(4 * img.dtype.num) - 1
+    print("normalized_val is", normalized_val)
 
-    print(img[0])
-    img = img * 65535
+    normalized_img = img/normalized_val
+
+    img = gamma_func(normalized_img)
+    # cv2.imshow('bbb.tif', img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+    img = img * normalized_val_uint16
     img = np.uint16(img)
 
     cv2.imwrite('tabako_half.tiff', img)
