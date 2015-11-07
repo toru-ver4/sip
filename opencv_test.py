658efa73adc873d8b3401041c474af5532b42160
Toru Yoshihara <toru.ver.11@gmail.com>
2015-11-07 22:48:35 +0900

    - add comment.

--- opencv_test.py
+++ opencv_test.py
@@ -6,13 +6,13 @@ helloqt.py
 PyQt5 で Hello, world!
 """
 
-File_name='bbb.dpx' 
 import os
 import sys
 import cv2
 import numpy as np
 from pgmagick import Image
 
+File_name='bbb.dpx' 
 normalized_val_uint16 = 65535
 
 def gamma_func(val):
@@ -42,36 +42,42 @@ def view_superwhite(img):
 
 if __name__ == '__main__':
 
+    # dpx形式のファイルをtiff(16bit)に変換
     tiff_image_name = conv_dpx10_to_tiff16(File_name)
 
+    # tiffのデータをread
     img = cv2.imread(tiff_image_name, cv2.IMREAD_ANYCOLOR|cv2.IMREAD_ANYDEPTH)
 
+    # 0..1 の範囲に正規化
     normalized_val = 2**(4 * img.dtype.num) - 1
-    print("normalized_val is", normalized_val)
-
     img             = img/normalized_val
-    print(img.shape)
+
+    # 画像を半分にリサイズ
     image_width  = img.shape[1]//2
     image_height = img.shape[0]//2
-    print(image_width, image_height)
     img_resize_half = cv2.resize(img, (image_width, image_height))
+
+    # 半分、64未満、940以上の画像を抽出
     img_half_level  = gamma_func(img_resize_half)
     img_black_area  = view_limited_black(img_resize_half)
     img_super_white = view_superwhite(img_resize_half)
     
-    # concatenate image
+    # 各画像を結合して1つの画像にする
     img_vcat1 = cv2.vconcat([img_resize_half, img_half_level])
     img_vcat2 = cv2.vconcat([img_black_area, img_super_white])
     img_hcat  = cv2.hconcat([img_vcat1, img_vcat2])
 
+    # 画像のプレビュー
     cv2.namedWindow("fullscreen", cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty("fullscreen", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow('bbb.tif', img_hcat)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    # 出力用に 0..1 → 0..65535 の変換を実施
     out_img = img_hcat * normalized_val_uint16
     out_img = np.uint16(out_img)
 
-    cv2.imwrite('tabako_half.tiff', out_img)
+    # 保存
+    cv2.imwrite('out.tiff', out_img)
     
