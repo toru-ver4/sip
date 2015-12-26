566e95a31b340899934c5c44765a5bdf5b0e44f0
Toru Yoshihara <toru.ver.11@gmail.com>
2015-12-26 16:00:21 +0900

    時間軸方向のフィルタをかけてみた。

--- opencv_movie.py
+++ opencv_movie.py
@@ -21,18 +21,22 @@ if __name__ == '__main__':
     capture = cv2.VideoCapture("nichijo_op.mp4")
     wait_time = int(1/29.97 * 1000)
 
+    ret, img_pre = capture.read()
+    if(ret != True):
+        print("source open error!")
+        sys.exit(0)
+
     while True:
-        ret, img_org = capture.read()
+        ret, img_now = capture.read()
         if(ret != True):
             break
-        edge_info = cv2.Canny(img_org, 40.0, 200.0)
-        img_edit = cv2.cvtColor(edge_info, cv2.COLOR_GRAY2RGB)
-#        img_edit = cv2.cvtColor(img_org, cv2.COLOR_RGB2YUV)
-
-        img_view = cv2.hconcat([img_org, img_edit])
+        img_edit = (img_pre * 0.4) + (img_now * 0.6)
+        img_edit = np.uint8(img_edit)
+        img_view = cv2.hconcat([img_now, img_edit])
         cv2.imshow("cam view", img_view)
-#        if cv2.waitKey(wait_time) >= 0:
-        if cv2.waitKey(20) >= 0:
+
+        img_pre = img_edit.copy()
+        if cv2.waitKey(1) >= 0:
             break
 
     cv2.destroyAllWindows()
