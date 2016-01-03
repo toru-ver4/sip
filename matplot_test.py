b224f4a66155f3d02b5138cb755a919f201894a0
Toru Yoshihara <toru.ver.11@gmail.com>
2016-01-03 13:11:44 +0900

    第一弾完成。まだグラフは汚い。見づらい。

--- matplot_test.py
+++ matplot_test.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 from matplotlib import font_manager
 from numpy.random import randn
 
-Picture_file_name='tabako.jpg' 
+#Picture_file_name='tabako.jpg' 
+Picture_file_name='hiasshuku.tiff' 
 
 if __name__ == '__main__':
 
@@ -22,15 +23,26 @@ if __name__ == '__main__':
     fig = plt.figure()
 
     # fig の中に複数のグラフを定義
-    ax1 = fig.add_subplot(2,2,1) # 引数はそれぞれ 横数、縦数、index 
-    ax2 = fig.add_subplot(2,2,2)
-    ax3 = fig.add_subplot(2,2,3)
+    rgb_hist = fig.add_subplot(4,1,1) # 引数はそれぞれ 横数、縦数、index 
+    r_hist = fig.add_subplot(4,1,2)
+    g_hist = fig.add_subplot(4,1,3)
+    b_hist = fig.add_subplot(4,1,4)
 
-    # ax に対して描画
-    ax1.hist(randn(300), bins=20, color='k', alpha=0.3)
-    ax2.plot(randn(50).cumsum(), 'k--' )
+    # ヒストグラムデータを準備
+    img = cv2.imread(Picture_file_name)
+    img_b, img_g, img_r = np.dsplit(img, 3)
+
+    # ヒストグラム作成
+    hist_bins = 256
+    hist_alpha = 0.5
+    rgb_hist.hist(img.flatten(), bins=hist_bins, color='k', alpha=hist_alpha)
+    r_hist.hist(img_r.flatten(), bins=hist_bins, color='r', alpha=hist_alpha)
+    g_hist.hist(img_g.flatten(), bins=hist_bins, color='g', alpha=hist_alpha)
+    b_hist.hist(img_b.flatten(), bins=hist_bins, color='b', alpha=hist_alpha)
 
     plt.show()
+
+
     
 
 
