c77a0ce1dc280b2a2fff7bae3c62309db0f68844
Toru Yoshihara <Toru Yoshihara>
2016-05-12 22:49:31 +0900

    なんか色々やってる。

--- kivy/pong_game/main.py
+++ kivy/pong_game/main.py
@@ -9,6 +9,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
@@ -20,12 +21,24 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
-    pass
+    
+    def update(self, dt):
+        self.ball.move()
+        
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 
 if __name__ == '__main__':
