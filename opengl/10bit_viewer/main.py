970040e8b809e0185bd096f6dbfba947fe6bbe30
toru yoshihara <toru.ver.11@gmail.com>
2016-08-21 16:05:53 +0900

    黒ウィンドウではなく、青ウィンドウを表示。

--- opengl/10bit_viewer/main.py
+++ opengl/10bit_viewer/main.py
@@ -8,14 +8,21 @@ import OpenGL.GLUT as glut
 
 
 def display():
-    pass
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+    gl.glFlush()
+
+
+def init():
+    gl.glClearColor(0.0, 0.0, 1.0, 1.0)
 
 
 if __name__ == '__main__':
     glut.glutInit(sys.argv)
     glut.glutInitWindowSize(400, 400)
+    glut.glutInitDisplayMode(glut.GLUT_RGBA)
     glut.glutCreateWindow(b"30bit demo")
     glut.glutDisplayFunc(display)
+    init()
     glut.glutMainLoop()
 
     # glut.glutInitDisplayString(b"red=10 green=10 blue=10 alpha=2")
