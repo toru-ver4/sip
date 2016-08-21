d1e10349e6f9a1496594b42a31b143526644419a
toru yoshihara <toru.ver.11@gmail.com>
2016-08-21 17:11:30 +0900

    ウィンドウサイズを変数化しました。

--- opengl/10bit_viewer/main.py
+++ opengl/10bit_viewer/main.py
@@ -6,20 +6,29 @@ import OpenGL.GL as gl
 import OpenGL.GLU as glu
 import OpenGL.GLUT as glut
 
+const_window_width = 400
+const_window_height = 800
+
 
 def display():
     gl.glClear(gl.GL_COLOR_BUFFER_BIT)
     gl.glBegin(gl.GL_POLYGON)
     gl.glColor3d(1.0, 0.0, 0.0)
     gl.glVertex2d(-0.9, -0.9)
-    gl.glColor3d(1.0, 1.0, 0.0)
     gl.glVertex2d(0.9, -0.9)
-    gl.glColor3d(0.0, 0.0, 1.0)
     gl.glVertex2d(0.9, 0.9)
-    gl.glColor3d(1.0, 1.0, 0.0)
     gl.glVertex2d(-0.9, 0.9)
     gl.glEnd()
     gl.glFlush()
+
+
+def resize(w, h):
+    gl.glViewport(0, 0, w, h)
+    gl.glLoadIdentity()
+
+    gl.glOrtho(-w/const_window_width, w/const_window_width,
+               -h/const_window_height, h/const_window_height,
+               -1.0, 1.0)
 
 
 def init():
@@ -28,10 +37,11 @@ def init():
 
 if __name__ == '__main__':
     glut.glutInit(sys.argv)
-    glut.glutInitWindowSize(400, 400)
+    glut.glutInitWindowSize(const_window_width, const_window_height)
     glut.glutInitDisplayMode(glut.GLUT_RGBA)
     glut.glutCreateWindow(b"30bit demo")
     glut.glutDisplayFunc(display)
+    glut.glutReshapeFunc(resize)
     init()
     glut.glutMainLoop()
 
