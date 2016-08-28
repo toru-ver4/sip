f4a76e764bccf2abe703474ee7b569aa7a1a9f99
Toru Yoshihara <toru.ver.11@gmail.com>
2016-08-29 08:21:13 +0900

    最大化表示に向けて黒枠追加のコードが完成。次は以下の2点にトライすること。   * 画像をセンターに置く   * フルスクリーンモードにする

--- opengl/10bit_viewer/main.py
+++ opengl/10bit_viewer/main.py
@@ -9,8 +9,8 @@ import OpenGL.GL as gl
 import OpenGL.GLU as glu
 import OpenGL.GLUT as glut
 
-const_window_width = 2048
-const_window_height = 1024
+const_window_width = 4096
+const_window_height = 2160
 global_x0 = 0
 global_y0 = 0
 texture = None
@@ -42,10 +42,6 @@ def init():
     gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
     img = get_img()
 
-    img = np.reshape(img, (img.shape[1], img.shape[0], img.shape[2]))
-    print(img.shape)
-    img = np.float32(img/0xFFFF)
-    print(img.dtype)
     gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB32F,
                     img.shape[0], img.shape[1], 0, gl.GL_RGB,
                     gl.GL_FLOAT, img.tostring())
@@ -56,7 +52,35 @@ def init():
 def get_img():
     img = cv2.imread('./figure/10bit_src.tiff',
                      cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
-    # print(img)
+
+    # フルスクリーン用に黒枠作成 ＆ 合成
+    # ------------------------------------
+    color_num = img.shape[2]
+    full_img = np.zeros((const_window_height, const_window_width, color_num),
+                        dtype=img.dtype)
+    full_img[0:img.shape[0], 0:img.shape[1], :] = img
+    img = full_img
+
+    # OpenCV は V --> H の並びなので、reshape して H --> V にする
+    # ------------------------------------
+    img = np.reshape(img, (img.shape[1], img.shape[0], img.shape[2]))
+    print(img.shape)
+    print(img.dtype)
+
+    # dtype から型の最大値を求めて正規化する
+    # ------------------------------------
+    try:
+        img_max_value = np.iinfo(img.dtype).max
+    except:
+        img_max_value = 1.0
+    print(img_max_value)
+
+    img = np.float32(img/img_max_value)
+
+    # cv2.imshow('preview', full_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
     return img[:, :, ::-1]
 
 
