5443a673f876c3aeef6d8129326c821b893b512a
toru yoshihara <toru.ver.11@gmail.com>
2016-11-03 12:57:56 +0900

    なんか更新。

--- PyCuda/3DLUT/code/pycuda_3dlut.py
+++ PyCuda/3DLUT/code/pycuda_3dlut.py
@@ -1,3 +1,4 @@
+import sys
 import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
@@ -8,12 +9,20 @@ import cv2
 const_matrix_param = np.array([0.2126, 0.7152, 0.0722])
 
 
-def mono_conv_matrix():
+def img_open_and_normalize(filename):
+    """
+    # 概要
+    以下の処理を実施。
 
-    # 加工元の画像データを準備
-    # -----------------------------------
-    filename = './figure/src_img.png'
+    * ファイルのオープン
+    * 正規化
+    """
+
     img = cv2.imread(filename, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
+    if img is None:
+        print("ERROR!\n{} is not found".format(filename))
+        sys.exit(1)
+
     try:
         img_max_value = np.iinfo(img.dtype).max
     except:
@@ -21,15 +30,102 @@ def mono_conv_matrix():
 
     img = np.float32(img/img_max_value)
 
+    return img
+
+
+def calc_block_and_grid(nx, ny, dimx, dimy):
+    """
+    # 概要
+    block_size(thread数)とgrid_size(block数)を計算する
+
+    # 参考資料
+    CUDA プロフェッショナルプログラミング P.62～P.64
+    """
+
+    block = (dimx, dimy, 1)
+    grid = ((nx + block[0] - 1) // block[0],
+            (ny + block[1] - 1) // block[1])
+
+    return block, grid
+
+
+def exec_3dlut():
+
+    # 加工元の画像データを準備
+    # -----------------------------------
+    filename = '../Matrix/figure/src_img.png'
+    img = img_open_and_normalize(filename=filename)
+
     # block数, thread数 の計算
     # ------------------------------------
     nx = img.shape[1]
     ny = img.shape[0]
-    dimx = 32
-    dimy = 32
-    block = (dimx, dimy, 1)
-    grid = ((nx + block[0] - 1) // block[0],
-            (ny + block[1] - 1) // block[1])
+    block, grid = calc_block_and_grid(nx=nx, ny=ny, dimx=32, dimy=32)
+
+    # GPUに画像データを転送
+    # ------------------------------------
+    img_gpu = cuda.mem_alloc(img.nbytes)
+    cuda.memcpy_htod(img_gpu, img[:, :, ::-1].tobytes())
+
+    # 3DLUTの次元数を設定
+    # ------------------------------------
+    
+
+    # カーネルの作成
+    # ------------------------------------
+    mod = SourceModule("""
+        __global__ void matrix_test(float *img, int nx, int ny)
+        {
+            float r, g, b;
+            unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
+            unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
+            unsigned int idx = iy * nx + ix;
+
+            if (ix < nx && iy < ny){
+                int r_idx = idx * 3;
+                int g_idx = idx * 3 + 1;
+                int b_idx = idx * 3 + 2;
+                r = img[r_idx];
+                g = img[g_idx];
+                b = img[b_idx];
+
+                img[r_idx] = r * 0.2126 + g * 0.7152 + b * 0.0722;
+                img[g_idx] = r * 0.2126 + g * 0.7152 + b * 0.0722;
+                img[b_idx] = r * 0.2126 + g * 0.7152 + b * 0.0722;
+            }
+        }
+        """)
+
+    # カーネルの実行
+    # ------------------------------------
+    func = mod.get_function("matrix_test")
+    func(img_gpu, np.uint32(nx), np.uint32(ny), grid=grid, block=block)
+
+    # 結果の取得
+    # ------------------------------------
+    img_result = np.empty_like(img)
+    cuda.memcpy_dtoh(img_result, img_gpu)
+    img_result = img_result[:, :, ::-1]
+
+    # 結果の表示
+    # ------------------------------------
+    cv2.imshow('preview', img_result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def mono_conv_matrix():
+
+    # 加工元の画像データを準備
+    # -----------------------------------
+    filename = '../Matrix/figure/src_img.png'
+    img = img_open_and_normalize(filename=filename)
+
+    # block数, thread数 の計算
+    # ------------------------------------
+    nx = img.shape[1]
+    ny = img.shape[0]
+    block, grid = calc_block_and_grid(nx=nx, ny=ny, dimx=32, dimy=32)
 
     # GPUに画像データを転送
     # ------------------------------------
@@ -81,3 +177,4 @@ def mono_conv_matrix():
 
 if __name__ == '__main__':
     pass
+
