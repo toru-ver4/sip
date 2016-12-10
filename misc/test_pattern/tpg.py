395e943a26716d347ea112c0906320a7390e3095
toru yoshihara <toru.ver.11@gmail.com>
2016-12-10 16:23:32 +0900

    window pattern 作成関数追加。

--- misc/test_pattern/tpg.py
+++ misc/test_pattern/tpg.py
@@ -19,6 +19,43 @@ def parameter_error_message(param_name):
     print('parameter "{}" is not valid.'.format(param_name))
 
 
+def gen_window_pattern(width=1920, height=1080,
+                       color=const_default_color, size=0.5, debug=True):
+    """
+    # 概要
+    Windowパターンを作成する。
+
+    # 詳細仕様
+    以下の図のように引数の size に応じて中央に window パターンを表示する。
+    size=1.0 は 全画面が window となる。
+
+    size = 0.6                          size = 1.0
+    +----------------------------+      +---------------------------+
+    |                            |      |                           |
+    |       +------------+       |      |                           |
+    |       |   window   |       |  =>  |          window           |
+    |       |            |       |  =>  |                           |
+    |       +------------+       |      |                           |
+    |                            |      |                           |
+    +----------------------------+      +---------------------------+
+    """
+
+    base_img = np.zeros((height, width, 3))
+    win_height = round(height * size)
+    win_width = round(width * size)
+    pt1 = (round((width - win_width) / 2.), round((height - win_height) / 2.))
+    pt2 = (pt1[0] + win_width, pt1[1] + win_height)
+
+    print(pt1, pt2)
+
+    cv2.rectangle(base_img, pt1, pt2, color, -1)
+
+    if debug:
+        cv2.imshow('preview', base_img[:, :, ::-1])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
 def gen_gradation_bar(width=1920, height=1080,
                       color=const_default_color, direction='h',
                       offset=0.0, debug=False):
@@ -62,8 +99,8 @@ def gen_gradation_bar(width=1920, height=1080,
 
 
 if __name__ == '__main__':
-    gen_gradation_bar(width=1920, height=1080, color=np.array([1.0, 0.7, 0.3]),
-                      direction='v', offset=1.0)
-    # gen_saturation_color_bar()
-    # add_profile()
-    # extract_profile()
+    gen_gradation_bar(width=1920, height=1080,
+                      color=np.array([1.0, 0.7, 0.3]),
+                      direction='v', offset=0.8, debug=False)
+    gen_window_pattern(width=1920, height=1080,
+                       color=np.array([1.0, 1.0, 1.0]), size=0.9, debug=True)
