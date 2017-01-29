054cc6ae66b55ed2d024f714af58c3d00aecb27b
toru yoshihara <toru.ver.11@gmail.com>
2017-01-29 18:10:10 +0900

    Color Patch を matplotlib で描いてみるが完成（たぶん）

--- SciPy/color_patch_analysis/check_color_patch_data.py
+++ SciPy/color_patch_analysis/check_color_patch_data.py
@@ -1,11 +1,17 @@
 import os
+import imp
 import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
-import pandas as pd
+import matplotlib.patches as patches
+import color_convert as ccv
 
 
 def show_color_patch_spectral_data():
+    """
+    # 概要
+    RIT で公開されている color patch のスペクトルデータをプロットする
+    """
     spectral_data = "./data/MacbethColorChecker_SpectralData.csv"
     data = np.loadtxt(spectral_data, delimiter=',', skiprows=3).T
 
@@ -33,11 +39,61 @@ def show_color_patch_spectral_data():
 
 
 def make_color_patch_image():
-    width = 200
-    height = 200
-    
+    """
+    # 概要
+    ColorChcker の color patch を表示(sRGB色域想定)
+    """
+    # csv ファイルから xyY データを取得
+    # --------------------------------
+    color_patch_xyY_data = "./data/ColorCheckerDataGretag.csv"
+    xyY_data = np.loadtxt(color_patch_xyY_data, delimiter=',', skiprows=1,
+                          usecols=(2, 3, 4))
+    xyY_data = xyY_data.reshape((1, xyY_data.shape[0], xyY_data.shape[1]))
+
+    # XYZに変換。さらに白のYが1.0となるように正規化
+    # と思ったが、なんかオーバーフローしたので 0.9掛けした
+    # --------------------------------
+    large_xyz_data = ccv.xyY_to_XYZ(xyY_data)
+    large_xyz_data /= np.max(xyY_data)
+    large_xyz_data *= np.max(xyY_data) / 100  # 0.9
+
+    # XYZ to RGB 変換を実施
+    # --------------------------------
+    rgb_large_xyz_matrix = ccv.get_rgb_to_xyz_matrix(gamut=ccv.const_sRGB_xy)
+    large_xyz_to_rgb_mtx = linalg.inv(rgb_large_xyz_matrix)
+    rgb_data = ccv.color_cvt(large_xyz_data, large_xyz_to_rgb_mtx)
+
+    # 範囲外の値のクリップ および 8bitで正規化
+    # --------------------------------
+    if (np.sum(rgb_data < 0) > 0) or (np.sum(rgb_data > 1) > 0):
+        print("Caution! Overflow has occured.")
+        rgb_data[rgb_data < 0] = 0
+        rgb_data[rgb_data > 1] = 1
+    rgb_data = (rgb_data ** (1/2.2))
+    rgb_data = np.uint8(np.round(rgb_data * 0xFF))
+
+    # plot
+    # ----------------------------------
+    v_num = 4
+    h_num = 6
+    plt.rcParams["font.size"] = 18
+    f, axarr = plt.subplots(v_num, h_num, sharex='col', sharey='row',
+                            figsize=(24, 16))
+    for idx in range(24):
+        color = "#{:02X}{:02X}{:02X}".format(rgb_data[0][idx][0],
+                                             rgb_data[0][idx][1],
+                                             rgb_data[0][idx][2])
+        h_idx = idx % h_num
+        v_idx = idx // h_num
+        axarr[v_idx, h_idx].add_patch(
+            patches.Rectangle(
+                (0, 0), 1.0, 1.0, facecolor=color
+            )
+        )
+    plt.show()
 
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    show_color_patch_spectral_data()
+    # show_color_patch_spectral_data()
+    make_color_patch_image()
---
+++ lib/color_convert.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+# 概要
+評価用のテストパターン作成ツール集
+
+# 使い方
+
+"""
+
+import cv2
+import numpy as np
+from scipy import linalg
+import matplotlib.pyplot as plt
+
+
+const_sRGB_xy = [[0.64, 0.33],
+                 [0.30, 0.60],
+                 [0.15, 0.06],
+                 [0.3127, 0.3290]]
+
+const_ntsc_xy = [[0.67, 0.33],
+                 [0.21, 0.71],
+                 [0.14, 0.08],
+                 [0.310, 0.316]]
+
+const_rec601_xy = const_ntsc_xy
+
+const_rec709_xy = const_sRGB_xy
+
+const_rec2020_xy = [[0.708, 0.292],
+                    [0.170, 0.797],
+                    [0.131, 0.046],
+                    [0.3127, 0.3290]]
+
+const_sRGB_xyz = [[0.64, 0.33, 0.03],
+                  [0.30, 0.60, 0.10],
+                  [0.15, 0.06, 0.79],
+                  [0.3127, 0.3290, 0.3583]]
+
+const_xyz_to_lms = [[0.8951000, 0.2664000, -0.1614000],
+                    [-0.7502000, 1.7135000, 0.0367000],
+                    [0.0389000, -0.0685000, 1.0296000]]
+
+const_rgb_to_large_xyz = [[2.7689, 1.7517, 1.1302],
+                          [1.0000, 4.5907, 0.0601],
+                          [0.0000, 0.0565, 5.5943]]
+
+const_d65_xy = [0.31271, 0.32902]
+const_d50_xy = [0.34567, 0.35850]
+
+const_rec601_y_coef = [0.2990, 0.5870, 0.1140]
+const_rec709_y_coef = [0.2126, 0.7152, 0.0722]
+
+
+def xy_to_xyz_internal(xy):
+    rz = 1 - (xy[0][0] + xy[0][1])
+    gz = 1 - (xy[1][0] + xy[1][1])
+    bz = 1 - (xy[2][0] + xy[2][1])
+    wz = 1 - (xy[3][0] + xy[3][1])
+
+    xyz = [[xy[0][0], xy[0][1], rz],
+           [xy[1][0], xy[1][1], gz],
+           [xy[2][0], xy[2][1], bz],
+           [xy[3][0], xy[3][1], wz]]
+
+    return xyz
+
+
+def get_rgb_to_xyz_matrix(gamut=const_sRGB_xy):
+
+    # まずは xyz 座標を準備
+    # ------------------------------------------------
+    if np.array(gamut).shape == (4, 2):
+        gamut = xy_to_xyz_internal(gamut)
+    elif np.array(gamut).shape == (4, 3):
+        pass
+    else:
+        print("============ Fatal Error ============")
+        print("invalid xy gamut parameter.")
+        print("=====================================")
+        sys.exit(1)
+
+    gamut_mtx = np.array(gamut)
+
+    # 白色点の XYZ を算出。Y=1 となるように調整
+    # ------------------------------------------------
+    large_xyz = [gamut_mtx[3][0]/gamut_mtx[3][1],
+                 gamut_mtx[3][1]/gamut_mtx[3][1],
+                 gamut_mtx[3][2]/gamut_mtx[3][1]]
+    large_xyz = np.array(large_xyz)
+
+    # Sr, Sg, Sb を算出
+    # ------------------------------------------------
+    s = linalg.inv(gamut_mtx[0:3]).T.dot(large_xyz)
+
+    # RGB2XYZ 行列を算出
+    # ------------------------------------------------
+    s_matrix = [[s[0], 0.0,  0.0],
+                [0.0,  s[1], 0.0],
+                [0.0,  0.0,  s[2]]]
+    s_matrix = np.array(s_matrix)
+    rgb2xyz_mtx = gamut_mtx[0:3].T.dot(s_matrix)
+
+    return rgb2xyz_mtx
+
+
+def color_cvt(img, mtx):
+    """
+    # 概要
+    img に対して mtx を適用する。
+    # 注意事項
+    例によって、RGBの並びを考えている。BGRの並びの場合は
+    img[:, :, ::-1] してから関数をコールすること。
+    """
+    try:
+        img_max = np.iinfo(img.dtype).max
+        img_min = np.iinfo(img.dtype).min
+    except ValueError:
+        img_max = np.finfo(img.dtype).max
+        img_min = np.finfo(img.dtype).min
+
+    r, g, b = np.dsplit(img, 3)
+    ro = r * mtx[0][0] + g * mtx[0][1] + b * mtx[0][2]
+    go = r * mtx[1][0] + g * mtx[1][1] + b * mtx[1][2]
+    bo = r * mtx[2][0] + g * mtx[2][1] + b * mtx[2][2]
+
+    out_img = np.dstack((ro, go, bo))
+
+    out_img[out_img < img_min] = img_min
+    out_img[out_img > img_max] = img_max
+
+    return out_img
+
+
+def xyY_to_XYZ(xyY):
+    """
+    # 概要
+    xyYからXYZを計算する
+
+    # 入力データ
+    numpy形式。shape = (1, N, 3)
+    """
+    small_x, small_y, large_y = np.dsplit(xyY, 3)
+    small_z = 1 - small_x - small_y
+    large_x = large_y / small_y * small_x
+    large_z = large_y / small_y * small_z
+
+    return np.dstack((large_x, large_y, large_z))
+
+
+if __name__ == '__main__':
+    pass
