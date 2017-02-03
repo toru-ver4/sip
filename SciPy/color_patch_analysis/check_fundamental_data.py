221b070a52a8e1ac23785e807000819aa8d2ec44
toru yoshihara <toru.ver.11@gmail.com>
2017-02-03 22:09:36 +0900

    平均と分散をヒストグラムに重ねてプロット

--- SciPy/color_patch_analysis/check_fundamental_data.py
+++ SciPy/color_patch_analysis/check_fundamental_data.py
@@ -173,12 +173,66 @@ def plot_color_patch_variance():
     plt.show()
 
 
-def get_color_patch_ave_val():
+def get_color_patch_average():
     """
     # 概要
-    カラーパッチの平均・分散の算出
+    カラーパッチの平均の算出。一応外れ値除外もするよ！
     """
-    pass
+    # カラーパッチデータ取得
+    coord_file = "./data/color_patch_coordinate.csv"
+    coord = np.loadtxt(coord_file, dtype=np.uint16, delimiter=',', skiprows=1, )
+    print(coord.shape)
+    img_file = "./figure/YAMADA_Z10-B.tif"
+    img = cv2.imread(img_file,
+                     cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
+
+    # color patch の領域をくり抜く
+    # ----------------------------
+    color_data = []
+    for idx in range(coord.shape[0]):
+        pt1 = (coord[idx][1], coord[idx][2])
+        pt2 = (coord[idx][5], coord[idx][6])
+        cv2.rectangle(img, pt1, pt2, (255, 255, 255))
+        color_data.append(img[coord[idx][2]:coord[idx][6],
+                              coord[idx][1]:coord[idx][5], :])
+
+    # まずは平均と分散を求める
+    # ------------------------
+    vari_array = []
+    ave_array = []
+    h_num = 3
+    v_num = 8
+    plt.rcParams["font.size"] = 16
+    f, axarr = plt.subplots(v_num, h_num, sharex='col', sharey='row',
+                            figsize=(20, 20))
+    for idx in range(v_num * h_num):
+        h_idx = idx % h_num
+        v_idx = idx // h_num
+        if v_idx == (v_num - 1):
+            axarr[v_idx, h_idx].set_xlabel("Video Level")
+        if h_idx == 0:
+            axarr[v_idx, h_idx].set_ylabel("Frequency")
+        axarr[v_idx, h_idx].set_xticks([20000, 40000, 60000])
+        p = color_data[idx]
+        p_b, p_g, p_r = np.dsplit(p, 3)
+        axarr[v_idx, h_idx].hist(p_r.flatten(), normed=False, bins=100,
+                                 color='red', alpha=0.5)
+        axarr[v_idx, h_idx].hist(p_g.flatten(), normed=False, bins=100,
+                                 color='green', alpha=0.5)
+        axarr[v_idx, h_idx].hist(p_b.flatten(), normed=False, bins=100,
+                                 color='blue', alpha=0.5)
+        rgb = [x for x in np.dsplit(color_data[idx], 3)]
+        b_v, g_v, r_v = [np.sqrt(np.var(x)) for x in rgb]
+        b_a, g_a, r_a = [np.average(x) for x in rgb]
+        axarr[v_idx, h_idx].plot([r_a-r_v, r_a-r_v], [0.0, 60000], 'r-')
+        axarr[v_idx, h_idx].plot([r_a+r_v, r_a+r_v], [0.0, 60000], 'r-')
+        axarr[v_idx, h_idx].plot([g_a-g_v, g_a-g_v], [0.0, 60000], 'g-')
+        axarr[v_idx, h_idx].plot([g_a+g_v, g_a+g_v], [0.0, 60000], 'g-')
+        axarr[v_idx, h_idx].plot([b_a-b_v, b_a-b_v], [0.0, 60000], 'b-')
+        axarr[v_idx, h_idx].plot([b_a+b_v, b_a+b_v], [0.0, 60000], 'b-')
+
+    plt.show()
+
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -186,6 +240,7 @@ if __name__ == '__main__':
     # make_color_patch_image()
     # plot_d_illuminant()
     # plot_color_patch_variance()
-    x = np.arange(27).reshape(3, 3, 3)
-    print(x)
-    print(np.mean(x, axis=3))
+    get_color_patch_average()
+    # x = np.arange(9).reshape(3, 3, 1)
+    # print(np.var(x))
+
