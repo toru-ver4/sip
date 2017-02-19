905cd8df7604d514e201ef7901f8de19ba9567fa
toru yoshihara <toru.ver.11@gmail.com>
2017-02-19 15:46:07 +0900

    複数パターンのcrosshatchを生成→保存するようにした。

--- signal_generation/test_pattern/check_test_pattern.py
+++ signal_generation/test_pattern/check_test_pattern.py
@@ -58,5 +58,195 @@ def make_rgbk_crosshatch(fg_array=fg_array_sample,
         tpg.preview_image(img[:, :, ::-1])
 
 
+def make_crosshatch(width=4096, height=2160,
+                    h_block=16, v_block=8,
+                    fragment_width=64, fragment_height=64,
+                    linewidth=1, linetype=cv2.LINE_AA,
+                    bg_color_array=tpg.const_gray_array_lower,
+                    fg_color_array=tpg.const_white_array,
+                    angle=30, debug=False):
+
+    # rgbcmykk pattern
+    # -----------------------------------------
+    bg_color_line = np.concatenate((tpg.const_black_array,
+                                    tpg.const_gray_array_lower))
+    bg_color_array = np.array([bg_color_line for x in range(v_block)])
+    after_shape = (bg_color_array.shape[0] * bg_color_array.shape[1],
+                   bg_color_array.shape[2])
+    bg_color_array = bg_color_array.reshape(after_shape)
+    fg_color_array = [tpg.const_red_grad_array_higher,
+                      tpg.const_red_array,
+                      tpg.const_green_grad_array_higher,
+                      tpg.const_green_array,
+                      tpg.const_blue_grad_array_higher,
+                      tpg.const_blue_array,
+                      tpg.const_cyan_grad_array_higher,
+                      tpg.const_cyan_array,
+                      tpg.const_magenta_grad_array_higher,
+                      tpg.const_magenta_array,
+                      tpg.const_yellow_grad_array_higher,
+                      tpg.const_yellow_array,
+                      tpg.const_gray_array_higher,
+                      tpg.const_white_array,
+                      tpg.const_gray_array_higher,
+                      tpg.const_white_array]
+    fg_color_array = np.array(fg_color_array)
+    after_shape = (fg_color_array.shape[0] * fg_color_array.shape[1],
+                   fg_color_array.shape[2])
+    fg_color_array = fg_color_array.reshape(after_shape)
+
+    img = tpg.make_multi_crosshatch(width=width, height=height,
+                                    h_block=h_block, v_block=v_block,
+                                    fragment_width=fragment_width,
+                                    fragment_height=fragment_height,
+                                    linewidth=linewidth,
+                                    linetype=linetype,
+                                    bg_color_array=bg_color_array,
+                                    fg_color_array=fg_color_array,
+                                    angle=angle, debug=debug)
+
+    return img
+
+
+def make_and_save_crosshatch():
+    # linewidth 1
+    # ----------------------------------------------
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=1, linetype=cv2.LINE_8,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=0, debug=False)
+    fname = "./figure/crosshatch_linewidth-1_antialiasing-off_angle-00.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=1, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=30, debug=False)
+    fname = "./figure/crosshatch_linewidth-1_antialiasing-on_angle-30.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=1, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=45, debug=False)
+    fname = "./figure/crosshatch_linewidth-1_antialiasing-on_angle-45.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=1, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=60, debug=False)
+    fname = "./figure/crosshatch_linewidth-1_antialiasing-on_angle-60.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    # linewidth 2
+    # ----------------------------------------------
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=2, linetype=cv2.LINE_8,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=0, debug=False)
+    fname = "./figure/crosshatch_linewidth-2_antialiasing-off_angle-00.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=2, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=30, debug=False)
+    fname = "./figure/crosshatch_linewidth-2_antialiasing-on_angle-30.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=2, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=45, debug=False)
+    fname = "./figure/crosshatch_linewidth-2_antialiasing-on_angle-45.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=64, fragment_height=64,
+                          linewidth=2, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=60, debug=False)
+    fname = "./figure/crosshatch_linewidth-2_antialiasing-on_angle-60.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    # linewidth 4
+    # ----------------------------------------------
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=96, fragment_height=96,
+                          linewidth=4, linetype=cv2.LINE_8,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=0, debug=False)
+    fname = "./figure/crosshatch_linewidth-4_antialiasing-off_angle-00.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=96, fragment_height=96,
+                          linewidth=4, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=30, debug=False)
+    fname = "./figure/crosshatch_linewidth-4_antialiasing-on_angle-30.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=96, fragment_height=96,
+                          linewidth=4, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=45, debug=False)
+    fname = "./figure/crosshatch_linewidth-4_antialiasing-on_angle-45.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=96, fragment_height=96,
+                          linewidth=4, linetype=cv2.LINE_AA,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=60, debug=False)
+    fname = "./figure/crosshatch_linewidth-4_antialiasing-on_angle-60.png"
+    cv2.imwrite(fname, img[:, :, ::-1])
+
+    # 実験用。debug=True で目視確認すること
+    # ---------------------------------------------
+    img = make_crosshatch(width=4096, height=2160,
+                          h_block=16, v_block=8,
+                          fragment_width=96, fragment_height=96,
+                          linewidth=4, linetype=cv2.LINE_8,
+                          bg_color_array=tpg.const_gray_array_lower,
+                          fg_color_array=tpg.const_white_array,
+                          angle=0, debug=False)
+
+
 if __name__ == '__main__':
-    make_rgbk_crosshatch(debug=True)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    make_and_save_crosshatch()
