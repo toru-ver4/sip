f962face3f7f83ec4824a785bdb52c11e43df1f2
toru yoshihara <toru.ver.11@gmail.com>
2017-03-19 16:27:00 +0900

    equal_division() 実装中。良いロジックが浮かばない。

--- lib/common.py
+++ lib/common.py
@@ -8,7 +8,7 @@
 """
 
 import numpy as np
-
+import math 
 
 def is_numpy_module(data):
     return type(data).__module__ == np.__name__
@@ -29,5 +29,44 @@ def is_correct_dtype(data, types={np.uint32, np.uint64}):
     return data.dtype.type in types
 
 
+def equal_devision(length, div_num):
+    """
+    # 概要
+    length を div_num で分割する。
+    端数が出た場合は誤差拡散法っぽく良い感じにする。
+
+    # 例(odd)
+    equal_devision(9, 9)  => [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    equal_devision(10, 9) => [2, 1, 1, 1, 1, 1, 1, 1, 1]
+    equal_devision(11, 9) => [2, 1, 1, 1, 2, 1, 1, 1, 1]
+    equal_devision(12, 9) => [2, 1, 1, 1, 2, 1, 1, 1, 2]
+    equal_devision(13, 9) => [2, 1, 2, 1, 2, 1, 1, 1, 2]
+    equal_devision(14, 9) => [2, 1, 2, 1, 2, 1, 2, 1, 2]
+    equal_devision(15, 9) => [2, 2, 2, 1, 2, 1, 2, 1, 2]
+    equal_devision(16, 9) => [2, 2, 2, 2, 2, 1, 2, 1, 2]
+    equal_devision(17, 9) => [2, 2, 2, 2, 2, 2, 2, 1, 2]
+
+    # 例(even)
+    equal_devision(10, 10) => [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    equal_devision(11, 10) => [2, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    equal_devision(12, 10) => [2, 1, 1, 1, 2, 1, 1, 1, 1, 1]
+    equal_devision(13, 10) => [2, 1, 1, 1, 2, 1, 1, 1, 2, 1]
+    equal_devision(14, 10) => [2, 1, 2, 1, 2, 1, 1, 1, 2, 1]
+    equal_devision(15, 10) => [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]
+    equal_devision(16, 10) => [2, 2, 2, 1, 2, 1, 2, 1, 2, 1]
+    equal_devision(17, 10) => [2, 2, 2, 2, 2, 1, 2, 1, 2, 1]
+    equal_devision(18, 10) => [2, 2, 2, 2, 2, 2, 2, 1, 2, 1]
+    equal_devision(19, 10) => [2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+    """
+    base = length // div_num
+    rest_val = length - (base * div_num)
+    ret_array = [base] * div_num
+    step = (div_num) // 2
+
+    are = int(math.floor(math.log2(div_num)))
+    are_list = [2 ** x for x in range(are)]
+    print(are_list)
+
+
 if __name__ == '__main__':
-    pass
+    equal_devision(17, 6)
