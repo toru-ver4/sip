66fd0c0646b6d4bafb8a895574cb1ec223a3beb6
toru yoshihara <toru.ver.11@gmail.com>
2017-04-09 09:45:02 +0900

    テストケース追加。下限値よりのパターンを増やした。

--- lib/tests/color_convert_test.py
+++ lib/tests/color_convert_test.py
@@ -63,15 +63,23 @@ class ColorConvertTestCase(unittest.TestCase):
         """
         lab_data1 = np.array([42.101, 53.378, 28.19]).reshape((1, 1, 3))
         lab_data2 = np.array([96.539, -0.425, 1.186]).reshape((1, 1, 3))
+        lab_data3 = np.array([20.461, -0.079, -0.973]).reshape((1, 1, 3))
+        lab_data4 = np.array([28.778, 14.179, -50.297]).reshape((1, 1, 3))
         expected1 = np.array([21.6315, 12.5654, 3.8476])
         expected2 = np.array([87.8151, 91.3135, 73.9795])
+        expected3 = np.array([2.9897, 3.1054, 2.6834])
+        expected4 = np.array([6.8605, 5.7520, 21.3801])
 
         result1 = ccv.lab_to_large_xyz(lab_data1, ccv.const_d50_large_xyz)
         result2 = ccv.lab_to_large_xyz(lab_data2, ccv.const_d50_large_xyz)
+        result3 = ccv.lab_to_large_xyz(lab_data3, ccv.const_d50_large_xyz)
+        result4 = ccv.lab_to_large_xyz(lab_data4, ccv.const_d50_large_xyz)
 
         for i in range(3):
             self.assertAlmostEqual(result1[0][0][i], expected1[i], 4)
             self.assertAlmostEqual(result2[0][0][i], expected2[i], 4)
+            self.assertAlmostEqual(result3[0][0][i], expected3[i], 4)
+            self.assertAlmostEqual(result4[0][0][i], expected4[i], 4)
 
     def test_large_xyz_to_lab(self):
         """
@@ -80,14 +88,23 @@ class ColorConvertTestCase(unittest.TestCase):
         """
         xyz_data1 = np.array([21.6315, 12.5654, 3.8476]).reshape((1, 1, 3))
         xyz_data2 = np.array([87.8151, 91.3135, 73.9795]).reshape((1, 1, 3))
+        xyz_data3 = np.array([2.9897, 3.1054, 2.6834]).reshape((1, 1, 3))
+        xyz_data4 = np.array([6.8605, 5.7520, 21.3801]).reshape((1, 1, 3))
         expected1 = np.array([42.1010, 53.3780, 28.1900])
         expected2 = np.array([96.5390, -0.4250, 1.1860])
+        expected3 = np.array([20.4612, -0.0803, -0.9726])
+        expected4 = np.array([28.7780, 14.1789, -50.2971])
 
         result1 = ccv.large_xyz_to_lab(xyz_data1, ccv.const_d50_large_xyz)
         result2 = ccv.large_xyz_to_lab(xyz_data2, ccv.const_d50_large_xyz)
+        result3 = ccv.large_xyz_to_lab(xyz_data3, ccv.const_d50_large_xyz)
+        result4 = ccv.large_xyz_to_lab(xyz_data4, ccv.const_d50_large_xyz)
+
         for i in range(3):
             self.assertAlmostEqual(result1[0][0][i], expected1[i], 3)
             self.assertAlmostEqual(result2[0][0][i], expected2[i], 3)
+            self.assertAlmostEqual(result3[0][0][i], expected3[i], 3)
+            self.assertAlmostEqual(result4[0][0][i], expected4[i], 3)
 
 
 if __name__ == '__main__':
