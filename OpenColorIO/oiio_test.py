ed47e71fcc350623f1931b8b97b4bd0f18f7b5de
toru yoshihara <toru.ver.11@gmail.com>
2017-06-10 12:39:46 +0900

    oiio のテストコード作成中。

--- OpenColorIO/oiio_test.py
+++ OpenColorIO/oiio_test.py
@@ -3,4 +3,14 @@ from OpenImageIO import ImageInput, ImageOutput
 from OpenImageIO import ImageBuf, ImageSpec, ImageBufAlgo
 
 if __name__ == '__main__':
-    print(oiio)
+    print(dir(oiio))
+    # t = oiio.TypeDesc(oiio.UINT8)
+    # print(t)
+    # print(oiio.TypeDesc.TypeInt)
+    input = oiio.ImageInput.open("test.png")
+    print(input.format_name())
+    spec = input.spec()
+    print(spec.width, spec.height)
+    input.close()
+    print(dir(ImageBufAlgo.colorconvert))
+
