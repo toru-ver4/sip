675bf52ccb38b8d2f7cf9b359bf895b95ffd97ea
Toru Yoshihara <toru.ver.11@gmail.com>
2018-06-10 12:06:50 +0900

    xy色度図の作成中。コメント追記した。

--- lib/test_pattern_generator2.py
+++ lib/test_pattern_generator2.py
@@ -8,6 +8,8 @@
 
 import os
 import cv2
+import plot_utility as pu
+import matplotlib.pyplot as plt
 import numpy as np
 from colour.plotting import chromaticity_diagram_plot_CIE1931
 from colour.colorimetry import CMFS, ILLUMINANTS
@@ -15,9 +17,13 @@ from colour.models import XYZ_to_xy, xy_to_XYZ, XYZ_to_RGB
 from colour.utilities import normalise_maximum
 from colour import models
 from scipy.spatial import Delaunay
-import matplotlib.pyplot as plt
-import matplotlib.tri as tri
 from scipy.ndimage.filters import convolve
+import imp
+imp.reload(pu)
+
+
+CMFS_NAME = 'CIE 1931 2 Degree Standard Observer'
+D65_WHITE = ILLUMINANTS[CMFS_NAME]['D65']
 
 
 def preview_image(img, order='rgb', over_disp=False):
@@ -34,24 +40,97 @@ def preview_image(img, order='rgb', over_disp=False):
     cv2.destroyAllWindows()
 
 
-def plot_chromaticity_diagram(antialiasing=True):
-    samples = 512
+def _get_cmfs_xy():
+    # 基本パラメータ設定
+    # ------------------
+    cmf = CMFS.get(CMFS_NAME)
+    d65_white = D65_WHITE
+
+    # 馬蹄形のxy値を算出
+    # --------------------------
+    cmf_xy = XYZ_to_xy(cmf.values, d65_white)
+
+    return cmf_xy
+
+
+def plot_chromaticity_diagram():
+    xy_image = get_chromaticity_image()
+    rate = 1.0
+    cmf_xy = _get_cmfs_xy()
+
+    ax1 = pu.plot_1_graph(fontsize=20,
+                          figsize=(8 * rate, 9 * rate),
+                          graph_title="Title",
+                          graph_title_size=None,
+                          xlabel=None, ylabel=None,
+                          axis_label_size=None,
+                          legend_size=17,
+                          xlim=(0, 0.8),
+                          ylim=(0, 0.9),
+                          xtick=[x * 0.1 for x in range(9)],
+                          ytick=[x * 0.1 for x in range(10)],
+                          xtick_size=10 * rate,
+                          ytick_size=10 * rate,
+                          linewidth=2,
+                          minor_xtick_num=2,
+                          minor_ytick_num=2)
+    ax1.plot(cmf_xy[..., 0], cmf_xy[..., 1], '-k')
+    ax1.plot((cmf_xy[-1, 0], cmf_xy[0, 0]), (cmf_xy[-1, 1], cmf_xy[0, 1]),
+             '-k')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def get_chromaticity_image(samples=1024, antialiasing=True):
+    """
+    xy色度図の馬蹄形の画像を生成する
+
+    Returns
+    -------
+    ndarray
+        rgb image.
+    """
+
+    """
+    色域設定。sRGBだと狭くて少し変だったのでBT.2020に設定。
+    若干色が薄くなるのが難点。暇があれば改良したい。
+    """
+    color_space = models.BT2020_COLOURSPACE
+
+    # 馬蹄形のxy値を算出
+    # --------------------------
+    cmf_xy = _get_cmfs_xy()
+
+    """
+    馬蹄の内外の判別をするために三角形で領域分割する(ドロネー図を作成)。
+    ドロネー図を作れば後は外積計算で領域の内外を判別できる（たぶん）。
+
+    なお、作成したドロネー図は以下のコードでプロット可能。
+    1点補足しておくと、```plt.triplot``` の第三引数は、
+    第一、第二引数から三角形を作成するための **インデックス** のリスト
+    になっている。[[0, 1, 2], [2, 4, 3], ...]的な。
+
+    ```python
+    plt.figure()
+    plt.triplot(xy[:, 0], xy[:, 1], triangulation.simplices.copy(), '-o')
+    plt.title('triplot of Delaunay triangulation')
+    plt.show()
+    ```
+    """
+    triangulation = Delaunay(cmf_xy)
+
+    """
+    ```triangulation.find_simplex()``` で xy がどのインデックスの領域か
+    調べることができる。戻り値が ```-1``` の場合は領域に含まれないため、
+    0以下のリストで領域判定の mask を作ることができる。
+    """
     xx, yy\
         = np.meshgrid(np.linspace(0, 1, samples), np.linspace(1, 0, samples))
     xy = np.dstack((xx, yy))
-    print(xy.shape)
-
-    cmf = CMFS.get('CIE 1931 2 Degree Standard Observer')
-    d65_white = ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D50']
-    cms_xy = XYZ_to_xy(cmf.values, d65_white)
-    triangulation = Delaunay(cms_xy)
-
-    # plt.figure()
-    # plt.triplot(xy[:, 0], xy[:, 1], triangulation.simplices.copy(), '-o')
-    # plt.title('triplot of Delaunay triangulation')
-    # plt.show()
-    # preview_image(xy)
     mask = (triangulation.find_simplex(xy) < 0).astype(np.float)
+
+    # アンチエイリアシングしてアルファチャンネルを滑らかに
+    # ------------------------------------------------
     if antialiasing:
         kernel = np.array([
             [0, 1, 0],
@@ -60,27 +139,38 @@ def plot_chromaticity_diagram(antialiasing=True):
         ]).astype(np.float)
         kernel /= np.sum(kernel)
         mask = convolve(mask, kernel)
-    mask = 1 - mask[:, :, np.newaxis]
-    # print(mask)
-    large_xyz = xy_to_XYZ(xy)
 
-    # 対象カラースペースの設定
+    # ネガポジ反転
+    # --------------------------------
+    mask = 1 - mask[:, :, np.newaxis]
+
+    # xy のメッシュから色を復元
     # ------------------------
-    color_space = models.BT2020_COLOURSPACE
-    illuminant_XYZ = d65_white
+    illuminant_XYZ = D65_WHITE
     illuminant_RGB = color_space.whitepoint
     chromatic_adaptation_transform = 'CAT02'
     large_xyz_to_rgb_matrix = color_space.XYZ_to_RGB_matrix
+    large_xyz = xy_to_XYZ(xy)
     rgb = XYZ_to_RGB(large_xyz, illuminant_XYZ, illuminant_RGB,
                      large_xyz_to_rgb_matrix,
                      chromatic_adaptation_transform)
+
+    """
+    そのままだとビデオレベルひ低かったりするので、
+    各ドット毎にRGB値を正規化＆最大化する。
+    """
     rgb = normalise_maximum(rgb, axis=-1)
+
     rgb = rgb ** (1/2.2)
+
+    # mask 適用
+    # -------------------------------------
     mask_rgb = np.dstack((mask, mask, mask))
-    # rgb[mask_rgb] = 0.0
-    rgb = rgb * mask_rgb
-    # preview_image(np.dstack((rgb, mask)))
-    preview_image(rgb)
+    rgb *= mask_rgb
+
+    # preview_image(rgb)
+
+    return rgb
 
 
 if __name__ == '__main__':
