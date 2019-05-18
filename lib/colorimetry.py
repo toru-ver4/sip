06ba7de38a75296c620795268a88b494ac507143
Toru Yoshihara <toru.ver.11@gmail.com>
2019-05-18 15:25:58 +0900

    new addition.

---
+++ lib/colorimetry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Colorimetry
+"""
+
+import os
+import numpy as np
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
