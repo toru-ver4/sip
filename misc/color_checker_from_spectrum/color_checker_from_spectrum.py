8ddf4104e73eb0e91cf94075ea5355e47c06d531
Toru Yoshihara <toru.ver.11@gmail.com>
2019-05-18 14:46:51 +0900

    前のコードが汚すぎたので作り直す。

---
+++ misc/color_checker_from_spectrum/color_checker_from_spectrum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# スペクトルからカラーチェッカー値を算出する
+
+## どうにかしたい点
+"""
+
+import os
+import numpy as np
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
