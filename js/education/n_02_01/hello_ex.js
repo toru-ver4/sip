6bd79c825a5c934aa9005cf600926e09c32b2f7c
Toru Yoshihara <toru.ver.11@gmail.com>
2019-08-11 20:44:24 +0900

    remove.

--- js/education/n_02_01/hello_ex.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-let result = 10 / 3;
-let str = `result = ${result}`;
-window.alert('こんにちは世界');
-console.log(str);
--- js/education/n_03/sample.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-let result = 10 / 3;
-let str = `result = ${result}`;
-console.log(str);
